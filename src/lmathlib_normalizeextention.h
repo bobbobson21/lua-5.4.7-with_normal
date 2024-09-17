7c6ed7b8efea9ecd00b0063b4a11c5532b981a95
kyle edwards <126681707+bobbobson21@users.noreply.github.com>
2024-09-17 21:13:35 +0100

    better normalize

--- src/lmathlib_normalizeextention.h
+++ src/lmathlib_normalizeextention.h
@@ -22,20 +22,21 @@
 
 static int math_NormaliseWithExponent(lua_State* L) {
     int ArgCount = lua_gettop(L);  /* number of arguments */
-    lua_Number Total = 0;
+    lua_Number Length = 0;
     lua_Number Exponent = l_mathop(fabs)(luaL_checknumber(L, 2));
 
 
     for (int i = 3; i <= ArgCount; i++)
     {
-        lua_Number ValToNum = l_mathop(fabs)(luaL_checknumber(L, i)); //gets total
-        Total += ValToNum;
+        lua_Number ValToNum = l_mathop(fabs)(luaL_checknumber(L, i)); //gets the valuse in dimantion I
+        Length += (ValToNum * ValToNum);
     }
+    Length = l_mathop(sqrt)Length; //gets the square root of the total resulting in us having the length of the vector
 
     for (int i = 3; i <= ArgCount; i++)
     {
         lua_Number ValToNum = (luaL_checknumber(L, i));
-        lua_pushnumber((ValToNum / Total) * Exponent); //end result
+        lua_pushnumber((ValToNum / Length) * Exponent); //end result
     }
 
     return 1;
@@ -43,18 +44,19 @@ static int math_NormaliseWithExponent(lua_State* L) {
 
 static int math_Normalise(lua_State* L) {
     int ArgCount = lua_gettop(L);  /* number of arguments */
-    lua_Number Total = 0;
+    lua_Number Length = 0;
 
     for (int i = 2; i <= ArgCount; i++)
     {
-        lua_Number ValToNum = l_mathop(fabs)(luaL_checknumber(L, i)); //gets total
-        Total += ValToNum;
+        lua_Number ValToNum = l_mathop(fabs)(luaL_checknumber(L, i)); //gets the valuse in dimantion I
+        Length += (ValToNum * ValToNum); //squares it
     }
+    Length = l_mathop(sqrt)Length; //gets the square root of the total resulting in us having the length of the vector
 
     for (int i = 2; i <= ArgCount; i++)
     {
         lua_Number ValToNum = (luaL_checknumber(L, i));
-        lua_pushnumber(ValToNum / Total); //end result
+        lua_pushnumber(ValToNum / Length); //end result
     }
 
     return 1;
